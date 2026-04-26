fe65f18032d8047f1fbdb0b2637997af636cfe3e
kingbruger333 <kingburger454545@gmail.com>
2026-04-26 19:34:42 +0300

    env update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import type { ReactNode } from "react";
 import { LeadChoiceProvider } from "@/components/lead-choice-provider";
 import "./globals.css";
@@ -20,6 +21,23 @@ export default function RootLayout({
     <html lang="ru" className="siteRoot">
       <body className="siteBody">
         <LeadChoiceProvider>{children}</LeadChoiceProvider>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108772003', 'ym');
+
+            ym(108772003, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/108772003" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
       </body>
     </html>
   );
